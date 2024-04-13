219378cde760ab44e2413de895490edab615ab3c
Erik <s194633@student.dtu.dk>
2024-04-13 10:53:28 +0200

    changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         # ModelCheckpoint setup
     checkpoint_callback = ModelCheckpoint(
         dirpath='/work3/s194633/ADLCV-weights',  # Set directory to save models
-        filename='best-maskrcnn',  # Example filename
+        filename='best-maskrcnn-no-copypaste',  # Example filename
         monitor='val_loss',  # Monitor validation loss
         mode='min',          # Save when validation loss decreases
         save_top_k=1,        # Save only the single best model
@@ -28,14 +28,14 @@ if __name__ == "__main__":
     # Hyperparameters 
     hparams = {
         'lr': 0.001,
-        'batch_size': 4
+        'batch_size': 8
     }
 
     # Example with basic Albumentations transforms - adjust as needed
     transform = A.Compose([
         A.RandomScale(scale_limit=(-0.9, 1), p=1), #LargeScaleJitter from scale of 0.1 to 2
-        # A.PadIfNeeded(256, 256, border_mode=0), #constant 0 border
-        # A.RandomCrop(256, 256),
+        A.PadIfNeeded(256, 256, border_mode=0), #constant 0 border
+        A.RandomCrop(256, 256),
         # A.HorizontalFlip(p=0.5),
         CopyPaste(blend=True, sigma=1, pct_objects_paste=0.5, p=1)
         ], bbox_params=A.BboxParams(format="coco",min_visibility=0.05)
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         train_file='coco_subset_annotations.json', 
         val_dir='/work3/s194633/val2017', 
         val_file='coco_subset_annotations_val.json', 
-        transforms=transform, 
+        transforms=None, 
         batch_size=hparams['batch_size']
     )
 
@@ -55,11 +55,11 @@ if __name__ == "__main__":
     model = MaskRCNNLightning(hparams)
 
 
-    wandb.init(name = "copy-paste", project="mask-rcnn")  # Initialize wandb project
+    wandb.init(name = "no-cp", project="mask-rcnn")  # Initialize wandb project
 
     trainer = pl.Trainer(
         accelerator="gpu", 
-        max_epochs=10, 
+        max_epochs=20, 
         logger=pl.loggers.WandbLogger(),  
         callbacks=[checkpoint_callback],  # Add the callback
         precision=16
--- modelling/dm.py
+++ modelling/dm.py
@@ -9,14 +9,14 @@ import albumentations as A
 class CocoDataModule(LightningDataModule):
     def __init__(self, train_dir, train_file, val_dir, val_file, transforms, batch_size):
         super().__init__()
-        #self.train_dataset = CustomCocoDetection(train_dir, train_file, transforms=transforms)
+        self.train_dataset = CustomCocoDetection(train_dir, train_file, transforms=transforms)
         self.val_dataset = CustomCocoDetection(val_dir, val_file, transforms=None)  # Example: no transforms for validation 
-        self.train_dataset = CocoDetectionCP(train_dir, train_file, transforms=transforms)
+        #self.train_dataset = CocoDetectionCP(train_dir, train_file, transforms=transforms)
 
         self.batch_size = batch_size
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=16,collate_fn=self.train_dataset.collate_fn, persistent_workers=True)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=8,collate_fn=self.train_dataset.collate_fn, persistent_workers=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=16, collate_fn=self.val_dataset.collate_fn, persistent_workers=True) 
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=8, collate_fn=self.val_dataset.collate_fn, persistent_workers=True) 
