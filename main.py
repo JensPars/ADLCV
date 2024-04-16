e804fa51f48f241dc97a49084c7a552d0f7c332b
Erik <s194633@student.dtu.dk>
2024-04-16 15:02:33 +0200

    segmentation stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pytorch_lightning as pl
-from modelling.model import MaskRCNNLightning
+from modelling.model import MaskRCNNLightning, DeepLabV3Lightning
 from pytorch_lightning.callbacks import ModelCheckpoint  # Import ModelCheckpoint
 from modelling.dm import CocoDataModule
 import sys
@@ -9,23 +9,28 @@ from simple_copy_paste.simple_copy_paste import CopyPaste
 import wandb
 import torch
 from torchvision.transforms import v2 as T
+from torchvision.transforms.functional import InterpolationMode
+
 
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 
-def get_transform():
+def get_transform(image=True):
     transforms = []
-    transforms.append(T.ToDtype(torch.float, scale=True))
-    transforms.append(T.ToPureTensor())
+    transforms.append(T.ToTensor())
+    if image:
+        transforms.append(T.Resize((256, 256), InterpolationMode.BILINEAR))
+    else:
+        transforms.append(T.Resize((256, 256), InterpolationMode.NEAREST))
     return T.Compose(transforms)
 
 if __name__ == "__main__":
-    num_classes = 9  # Number of classes in the dataset
+    num_classes = 1  # Number of classes in the dataset
 
         # ModelCheckpoint setup
     checkpoint_callback = ModelCheckpoint(
-        dirpath='/work3/s194633/ADLCV-weights',  # Set directory to save models
-        filename='best-maskrcnn-no-copypaste',  # Example filename
+        dirpath='models',  # Set directory to save models
+        filename='best-seg-no-copypaste',  # Example filename
         monitor='val_loss',  # Monitor validation loss
         mode='min',          # Save when validation loss decreases
         save_top_k=1,        # Save only the single best model
@@ -34,41 +39,44 @@ if __name__ == "__main__":
 
     # Hyperparameters 
     hparams = {
-        'lr': 0.005,
-        'batch_size': 8
+        'lr': 2e-5,
+        'batch_size': 2
     }
 
-    # Example with basic Albumentations transforms - adjust as needed
-    transform = A.Compose([
-        A.RandomScale(scale_limit=(-0.9, 1), p=1), #LargeScaleJitter from scale of 0.1 to 2
-        A.PadIfNeeded(256, 256, border_mode=0), #constant 0 border
-        A.RandomCrop(256, 256),
-        # A.HorizontalFlip(p=0.5),
-        CopyPaste(blend=True, sigma=1, pct_objects_paste=0.5, p=1)
-        ], bbox_params=A.BboxParams(format="coco",min_visibility=0.05)
-    )
+    # # Example with basic Albumentations transforms - adjust as needed
+    # transform = A.Compose([
+    #     A.RandomScale(scale_limit=(-0.9, 1), p=1), #LargeScaleJitter from scale of 0.1 to 2
+    #     A.PadIfNeeded(256, 256, border_mode=0), #constant 0 border
+    #     A.RandomCrop(256, 256),
+    #     # A.HorizontalFlip(p=0.5),
+    #     CopyPaste(blend=True, sigma=1, pct_objects_paste=0.5, p=1)
+    #     ], bbox_params=A.BboxParams(format="coco",min_visibility=0.05)
+    # )
 
     # Dataset setup 
     data_module = CocoDataModule(
-        train_dir="/work3/s194633/train2017",
-        train_file='coco_subset_annotations.json', 
-        val_dir='/work3/s194633/val2017', 
-        val_file='coco_subset_annotations_val.json', 
-        transforms=get_transform(), 
-        batch_size=hparams['batch_size']
+        train_dir="data/val2017",
+        train_file='bear_subset_annotations_val.json', 
+        val_dir='data/val2017', 
+        val_file='bear_subset_annotations_val.json', 
+        transforms_im=get_transform(image=True),
+        transforms_mask=get_transform(image=False), 
+        batch_size=hparams['batch_size'],
+
     )
 
     # Model
-    model = MaskRCNNLightning(hparams)
+    model = DeepLabV3Lightning(hparams, num_classes=num_classes)
 
 
-    wandb.init(name = "no-cp", project="mask-rcnn")  # Initialize wandb project
+    wandb.init(name = "no-cp-segmentation", project="segmentation-copy-paste")  # Initialize wandb project
 
     trainer = pl.Trainer(
-        accelerator="gpu", 
+        accelerator="cpu", 
         max_epochs=20, 
         logger=pl.loggers.WandbLogger(),  
         callbacks=[checkpoint_callback],  # Add the callback
-        precision=16
+        precision=16,
+        profiler="advanced"
     ) 
     trainer.fit(model, datamodule=data_module)  # Fit the model
--- modelling/dm.py
+++ modelling/dm.py
@@ -2,21 +2,21 @@ from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
 from simple_copy_paste.simple_copy_paste import copy_paste_class, CopyPaste
 from simple_copy_paste.coco import CocoDetectionCP 
-from simple_copy_paste.coco import CustomCocoDetection
+from simple_copy_paste.coco import CustomCocoDetection, CustomCocoSegmentation
 import albumentations as A
 
 
 class CocoDataModule(LightningDataModule):
-    def __init__(self, train_dir, train_file, val_dir, val_file, transforms, batch_size):
+    def __init__(self, train_dir, train_file, val_dir, val_file, transforms_im, transforms_mask, batch_size):
         super().__init__()
-        self.train_dataset = CustomCocoDetection(train_dir, train_file, transforms=transforms)
-        self.val_dataset = CustomCocoDetection(val_dir, val_file, transforms=transforms)
+        self.train_dataset = CustomCocoSegmentation(train_dir, train_file, transforms_im=transforms_im, transforms_mask=transforms_mask)
+        self.val_dataset = CustomCocoSegmentation(val_dir, val_file, transforms_im=transforms_im, transforms_mask=transforms_mask)
         #self.train_dataset = CocoDetectionCP(train_dir, train_file, transforms=transforms)
 
         self.batch_size = batch_size
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=8,collate_fn=self.train_dataset.collate_fn, persistent_workers=True)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=1,persistent_workers=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=8, collate_fn=self.val_dataset.collate_fn, persistent_workers=True) 
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=1,persistent_workers=True) 
